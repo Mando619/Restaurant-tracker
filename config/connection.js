9b5d271dc744f8d2f4e14b50b791a41e739f1abf
Mando Estrada <libryum33@hotmail.com>
2020-10-14 19:11:04 -0700

    updating and deleting files.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 
-var connection;
+
 if (process.env.JAWSDB_URL) {
     // Database is JawsDB on Heroku
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     // Database is local
-    var connection = mysql.createConnection({
+     connection = mysql.createConnection({
         port: 3306,
         host: 'localhost',
         user: 'root',
@@ -15,14 +15,7 @@ if (process.env.JAWSDB_URL) {
     })
 };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
 
 
@@ -31,6 +24,7 @@ module.exports = connection;
 
 //    mysql://utb9wkmamlbgl6mx:s6sba1fnnchla6cd@y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q2cs3pwgw0axv9ok
 
+//JAWSDB_URL='mysql://utb9wkmamlbgl6mx:s6sba1fnnchla6cd@y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q2cs3pwgw0axv9ok'
 
 
 
